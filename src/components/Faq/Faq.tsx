f6e53bc51e65b46285e41413a2ecb4fc1a5899bf
Andrey Kudinov <andrey.kudinov@avaloninnovations.com.au>
2023-04-19 21:42:03 +0400

    Update Faq section

--- src/components/Faq/Faq.tsx
+++ src/components/Faq/Faq.tsx
@@ -1,3 +1,5 @@
+import cn from 'classnames';
+
 import { FaqItem } from '../FaqItem/FaqItem';
 import styles from './Faq.module.css';
 
@@ -12,8 +14,8 @@ interface IProps {
 
 export const Faq = ({ items }: IProps) => (
   <section className="container">
-    <h2 className={styles.title}>
-      Часто задаваемые <span>вопросы</span>
+    <h2 className={cn(styles.title, 'heading2')}>
+      Часто задаваемые <span className="primary-color">вопросы</span>
     </h2>
     <div className={styles.accordion}>
       {items?.map((item, idx) => (
