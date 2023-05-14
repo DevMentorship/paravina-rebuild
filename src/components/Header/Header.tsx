a3c5f98795310ea05e5296fc0e39a3814cb99879
Anna Hrulckova <annahrulkova@yandex.ru>
2023-05-14 18:27:54 +0400

    добавлен логотип в header  и стили для header

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,5 +1,5 @@
 import Link from 'next/link';
-
+import Image from 'next/image';
 import styles from '@/components/Header/Header.module.css';
 
 const pages = [
@@ -10,10 +10,15 @@ const pages = [
 
 export const Header = () => (
   <header className={styles.header}>
-    {pages.map(({ label, href }, index) => (
-      <Link href={href} key={index} className={styles.link}>
-        {label}
-      </Link>
-    ))}
+    <div className={styles.nav}>
+      {pages.map(({ label, href }, index) => (
+        <Link href={href} key={index} className={styles.link}>
+          {label}
+        </Link>
+      ))}
+    </div>
+    <div className={styles.logo}>
+      <Image src={'header-logo.svg'} width={100} height={100} alt="лого" className={styles.img}></Image>
+    </div>
   </header>
 );
