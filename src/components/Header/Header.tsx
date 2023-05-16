eb1903835351378f4ca8dec289d9219cb081c867
Andrey Kudinov <andrey.kudinov@avaloninnovations.com.au>
2023-05-16 15:18:50 +0400

    Update header a bit

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,5 +1,6 @@
-import Link from 'next/link';
 import Image from 'next/image';
+import Link from 'next/link';
+
 import styles from '@/components/Header/Header.module.css';
 
 const pages = [
@@ -10,15 +11,17 @@ const pages = [
 
 export const Header = () => (
   <header className={styles.header}>
-    <div className={styles.nav}>
-      {pages.map(({ label, href }, index) => (
-        <Link href={href} key={index} className={styles.link}>
-          {label}
-        </Link>
-      ))}
-    </div>
-    <div className={styles.logo}>
-      <Image src={'header-logo.svg'} width={100} height={100} alt="лого" className={styles.img}></Image>
+    <div className="container">
+      <div className={styles.logo}>
+        <Image src={'header-logo.svg'} width={100} height={100} alt="лого" className={styles.img} />
+      </div>
+      <div className={styles.nav}>
+        {pages.map(({ label, href }, index) => (
+          <Link href={href} key={index} className={styles.link}>
+            {label}
+          </Link>
+        ))}
+      </div>
     </div>
   </header>
 );
