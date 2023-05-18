c99d671eaf1874463f6e5c039efa7ec30d0bcca4
kozurasergj <kozura.sergj33@gmail.com>
2023-05-18 15:37:41 +0300

    Added 2 blocks and responsiveness

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -16,39 +16,45 @@ export const Footer = () => (
           {label}
         </Link>
       ))} */}
-    <div className={styles.additional}>
-      <div className={styles.adress}>
-        <p> АДРЕС</p>
-        <p>г. Самара, ул. Ново-Садовая, 164а</p>
-        <p>(рядом с магазином «Турист»)</p>
+    <section className={styles.container}>
+      <div className={styles.additional}>
+        <div className={styles.adress}>
+          <p> АДРЕС</p>
+          <p>г. Самара, ул. Ново-Садовая, 164а</p>
+          <p>(рядом с магазином «Турист»)</p>
+        </div>
+        <div className={styles.schedule}>
+          <p>РЕЖИМ РАБОТЫ</p>
+          <p>Пн-сб: с 9 до 20 часов</p>
+          <p>Вс: выходной день</p>
+        </div>
+        <div className={styles.phone}>
+          <p> ТЕЛЕФОН</p>
+          <Link href="/" className={styles.link}>
+            +7 927 692-25-01
+          </Link>
+        </div>
+        <div className={styles.documentation}>
+          <p>ДОКУМЕНТЫ</p>
+          <Link href="/" className={styles.link}>
+            Лицензии
+          </Link>
+          <p>Прейскурант цен</p>
+        </div>
       </div>
-      <div className={styles.schedule}>
-        <p>РЕЖИМ РАБОТЫ</p>
-        <p>Пн-сб: с 9 до 20 часов</p>
-        <p>Вс: выходной день</p>
-      </div>
-      <div className={styles.phone}>
-        <p> ТЕЛЕФОН</p>
-        <Link href="/" className={styles.link}>
-          +7 927 692-25-01
+      <div className={styles.subscribe}>
+        <p>ПОДПИШИТЕСЬ</p>
+        <Link href="/">
+          <Image src="/vk.svg" width="90" height="90" alt="VK" />
+        </Link>
+        <Link href="/">
+          <Image src="/youtube.svg" width="90" height="90" alt="YouTub" />
         </Link>
       </div>
-      <div className={styles.documentation}>
-        <p>ДОКУМЕНТЫ</p>
-        <Link href="/" className={styles.link}>
-          Лицензии
-        </Link>
-        <p>Прейскурант цен</p>
-      </div>
-    </div>
-    <div className={styles.subscribe}>
-      <p>ПОДПИШИТЕСЬ</p>
-      <Link href="/">
-        <Image src="/vk.svg" width="90" height="90" alt="VK" />
-      </Link>
-      <Link href="/">
-        <Image src="/youtube.svg" width="90" height="90" alt="YouTub" />
-      </Link>
-    </div>
+    </section>
+    <Link href="/" className={styles.link}>
+      Политика конфиденциальности
+    </Link>
+    <p className={styles.consultation}>Имеются противопоказания. Необходима консультация специалиста.</p>
   </footer>
 );
