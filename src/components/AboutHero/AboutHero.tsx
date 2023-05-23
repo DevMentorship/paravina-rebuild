002d5022c8f0fc0e23a8884276cfb2b6605a8b87
Andrey Kudinov <andrey.kudinov@avaloninnovations.com.au>
2023-05-23 08:42:56 +0400

    Update a bit

--- src/components/AboutHero/AboutHero.tsx
+++ src/components/AboutHero/AboutHero.tsx
@@ -7,8 +7,11 @@ export const AboutHero = () => (
   <section>
     <div className={styles.about}>
       <div className={cn(styles.title, 'third-color')}>
-        <h3 className={cn(styles.title_yellow, 'heading1')}>Давайте</h3>
-        <h3 className="heading1">знакомиться!</h3>
+        <h3 className="heading1">
+          <span className="primary-color">Давайте</span>
+          <br />
+          знакомиться!
+        </h3>
         <p className="heading2">
           Ярослав Опритов и Екатерина Паравина — мы основатели первой в Самаре авторской стоматологии. Ярослав отвечает
           за за то, чтобы всё и всегда в клинике работало как часы, а Екатерина творит магию красоты, вдохновляет
@@ -21,7 +24,7 @@ export const AboutHero = () => (
         src={`/about-hero-image/about_hero.png`}
         alt="about-image"
         width={1920}
-        height={1197}
+        height={1200}
       />
     </div>
   </section>
