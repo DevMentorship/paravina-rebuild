a21be5b107ae1bed73a68d3b0652a7f8fa96ac3e
Anton Potapov <panton160786@gmail.com>
2023-05-28 14:36:39 +0930

    Some changed according to comments

--- src/components/AboutHero/AboutHero.tsx
+++ src/components/AboutHero/AboutHero.tsx
@@ -6,15 +6,24 @@ import { IAbout } from '@/pages/about';
 
 import styles from './AboutHero.module.css';
 
-export const AboutHero = (props: IAbout) => (
-  <section>
-    <div className={styles.about}>
-      <div className={cn(styles.title, 'third-color')}>
-        <h3 className={cn(styles.title_yellow, 'heading1')}>{props.main_title}</h3>
-        <h3 className="heading1">{props.second_title}</h3>
-        <p className="heading2">{props.descr}</p>
+export const AboutHero = ({ title, descr, image }: IAbout) => {
+  const dividedTitle = title.split(' ');
+  // wrap to the {}, and add return() before section
+  return (
+    <section>
+      <div className={styles.about}>
+        <div className={cn(styles.title, 'third-color')}>
+          <h3 className="heading1">
+            <span className="primary-color">
+              {dividedTitle[0]}
+              <br />
+            </span>
+            {dividedTitle[1]}
+          </h3>
+          <p className="heading2">{descr}</p>
+        </div>
+        <Image className={styles.image} src={urlFor(image).url()} alt="image" width={1920} height={1197} />
       </div>
-      <Image className={styles.image} src={urlFor(props.image).url()} alt="about-image" width={1920} height={1197} />
-    </div>
-  </section>
-);
+    </section>
+  );
+};
--- src/pages/about.tsx
+++ src/pages/about.tsx
@@ -8,15 +8,14 @@ import { client } from '@/lib/client';
 export interface IAbout {
   _id: string;
   image: TypedObject;
-  main_title: string;
-  second_title: string;
+  title: string;
   descr: string;
 }
 
 export default function About(props: IAbout) {
   return (
     <>
-      <AboutHero key={props._id} {...props} />
+      <AboutHero {...props} />
       <Team />
       <Reviews />
     </>
@@ -25,11 +24,10 @@ export default function About(props: IAbout) {
 
 export const getStaticProps = async () => {
   const query = `{
-    "aboutData": *[_type == "aboutHero"]  {image, main_title, second_title, descr, _id}
+    "aboutData": *[_type == "aboutHero"]  {image, title, descr, _id}
   }`;
   const { aboutData } = await client.fetch(query);
 
   const data = aboutData[0];
-
   return { props: data };
 };
