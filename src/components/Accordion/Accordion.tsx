385201207ef1154fd7727bc97a9577b0da7c8db6
unknown <Lihtinmann88@yandex.ru>
2023-06-05 21:07:09 +0300

    PricesTab component was added

--- src/components/Accordion/Accordion.tsx
+++ src/components/Accordion/Accordion.tsx
@@ -1,5 +1,4 @@
 import cn from 'classnames';
-import { useEffect, useRef, useState } from 'react';
 
 import { AccordionItem, IAccordionItem } from '../AccordionItem/AccordionItem';
 import styles from './Accordion.module.css';
@@ -7,44 +6,17 @@ import styles from './Accordion.module.css';
 export interface IAccordion {
   items: IAccordionItem[];
   title: string;
-  isVisible: boolean;
+  isVisible?: boolean;
+  className?: string | undefined;
 }
 
-export const Accordion = ({ items, title, isVisible }: IAccordion) => {
-  const [isOpened, setIsOpened] = useState<boolean>(isVisible ? true : false);
-  const [maxHeight, setMaxHeight] = useState(0);
-  const ref = useRef<HTMLDivElement>(null);
-
-  useEffect(() => {
-    if (isOpened) {
-      const contentEl = ref.current as HTMLDivElement;
-      setMaxHeight(contentEl.scrollHeight);
-    } else {
-      setMaxHeight(0);
-    }
-  }, [isOpened]);
-
-  return (
-    <section className="container">
-      {isVisible ? (
-        <h2 className={cn(styles.title, 'heading2')}>{title}</h2>
-      ) : (
-        <button
-          onClick={() => (isOpened ? setIsOpened(false) : setIsOpened(true))}
-          className={cn(styles.btn, 'heading2')}
-        >
-          {title}
-        </button>
-      )}
-      <div
-        ref={ref}
-        className={cn(styles.accordion, isOpened ? styles.visible : styles.invisible)}
-        style={{ maxHeight }}
-      >
-        {items?.map((item, idx) => (
-          <AccordionItem key={idx} data={item} tabIndex={isOpened ? 0 : -1} />
-        ))}
-      </div>
-    </section>
-  );
-};
+export const Accordion = ({ items, title, className }: IAccordion) => (
+  <section className={cn('container', className)}>
+    <h2 className={cn(styles.title, 'heading2')}>{title}</h2>
+    <div className={styles.accordion}>
+      {items?.map((item, idx) => (
+        <AccordionItem key={idx} data={item} />
+      ))}
+    </div>
+  </section>
+);
---
+++ src/components/PricesTab/PricesTab.tsx
@@ -0,0 +1,43 @@
+import cn from 'classnames';
+import { useState } from 'react';
+
+import { Accordion, IAccordion } from '../Accordion/Accordion';
+import styles from './PricesTab.module.css';
+
+interface IPricesTabProps {
+  prices: IAccordion[];
+}
+
+export const PricesTab = ({ prices }: IPricesTabProps) => {
+  const [selectedCategory, setSelectedCategory] = useState<string>('Детская стоматология');
+
+  return (
+    <section className="container">
+      <div className={styles.wrapper}>
+        <div className={styles['triggers-wrapper']}>
+          <div className={styles.triggers}>
+            {prices.map((price, index) => (
+              <button
+                key={index}
+                className={cn(styles.trigger, selectedCategory === price.title && styles['trigger--active'])}
+                onClick={() => setSelectedCategory(price.title)}
+              >
+                {price.title}
+              </button>
+            ))}
+          </div>
+        </div>
+      </div>
+      <div className={styles.items}>
+        {prices.map((price, index) => (
+          <Accordion
+            key={index}
+            items={price.items}
+            title={price.title}
+            className={cn(styles['item-invisible'], price.title !== selectedCategory && styles['item-visible'])}
+          />
+        ))}
+      </div>
+    </section>
+  );
+};
